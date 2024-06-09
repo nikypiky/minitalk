ff46ce9e5b5f4aa3c2cc3d7c9efaef2d8652c5cc
Your Name <naldorsc2@gmail.com>
2024-06-09 18:10:19 +0200

    starting to test sending bits

--- client.c
+++ client.c
@@ -2,17 +2,51 @@
 
 int	main (int argc, char **argv)
 {
-	// printf("argc = %i argv = %s", argc, argv[1]);
-	if (argc != 2)
+	int		i;
+	int		pid;
+	char	bit_test;
+	char	test = 'a';
+
+	bit_test = 0b00000001;
+	pid = miniatoi(argv[argc - 1]);
+	i = 0;
+	// if (argc != 3)
+	// {
+	// 	write(1, "Please input server PID and message.\n", 26);
+	// 	return(1);
+	// }
+	while (i <= 8)
 	{
-		write(1, "Please input server PID.\n", 26);
-		return(1);
+		if ((test & bit_test) != 1)
+		{
+			if (kill(pid, SIGUSR1) == -1)
+			{
+				perror("Error sending signal"); // Prints the error message and reason
+				return 1;
+			}
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+			{
+				perror("Error sending signal"); // Prints the error message and reason
+				return 1;if (kill(pid, SIGUSR2) == -1)
+			{
+				perror("Error sending signal"); // Prints the error message and reason
+				return 1;
+			}
+			}
+		}
+		test = test >> 1;
+		i++;
 	}
-	if (kill(miniatoi(argv[1]), SIGUSR1) == -1) {
+	if (kill(pid, SIGUSR1) == -1)
+	{
 		printf("error %s\n", argv[1]);
-        perror("Error sending signal"); // Prints the error message and reason
-        return 1;
-    }	// printf("client %i\n", miniatoi("1234"));
+		perror("Error sending signal"); // Prints the error message and reason
+		return 1;
+	}
+    	printf("client %i\n", miniatoi("1234"));
 }
 
 
