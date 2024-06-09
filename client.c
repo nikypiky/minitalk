7457b7c1fa876fc13550158051f41840fa75498e
Your Name <naldorsc2@gmail.com>
2024-06-09 16:21:25 +0200

    signal sending now works + fixing write_pid

--- client.c
+++ client.c
@@ -1,8 +1,18 @@
 #include "include/minitalk.h"
 
-int	main (int argc, char** argv)
+int	main (int argc, char **argv)
 {
-	kill(miniatoi(argv[argc - 1]), SIGUSR1);
-	// printf("client %i\n", miniatoi("1234"));
+	// printf("argc = %i argv = %s", argc, argv[1]);
+	if (argc != 2)
+	{
+		write(1, "Please input server PID.\n", 26);
+		return(1);
+	}
+	if (kill(miniatoi(argv[1]), SIGUSR1) == -1) {
+		printf("error %s\n", argv[1]);
+        perror("Error sending signal"); // Prints the error message and reason
+        return 1;
+    }	// printf("client %i\n", miniatoi("1234"));
 }
+
 
