977603d8dbc1d841a17cfaebb209c735924f6c48
Your Name <naldorsc2@gmail.com>
2024-06-25 05:12:00 +0200

    commit

---
+++ utils/send.c
@@ -0,0 +1,69 @@
+#include "../include/minitalk.h"
+
+// int send_var (void *var, int pid)
+// {
+// 	int	bit_test;
+// 	int	j;
+// 	int	var_size;
+
+// 	var_size = sizeof(var) * 8;
+// 	bit_test = 2 ^ (var_size - 1);
+// 	j = 0;
+// 	if (var_size == 8)
+// 		(char *)var;
+// 	else
+// 		(int *)var;
+// 	while (j < var_size * 8)
+// 	{
+// 		if ((var & bit_test) == 0)
+// 			kill(pid, SIGUSR1);
+// 		else
+// 			kill(pid, SIGUSR2);
+// 		j++;
+// 		bit_test = bit_test >> 1;
+// 		sleep(0.1);
+// 	}
+// 	return (0);
+// }
+
+int send_char (char c, int pid)
+{
+	int	bit_test;
+	int	j;
+
+	bit_test = 128;
+	j = 0;
+
+	while (j < 8)
+	{
+		if ((c & bit_test) == 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		j++;
+		bit_test = bit_test >> 1;
+		sleep(1);
+	}
+	return (0);
+}
+
+int send_int (int i, int pid)
+{
+	unsigned int	bit_test;
+	int				j;
+
+	bit_test = 2147483648;
+	j = 0;
+
+	while (j < 32)
+	{
+		if ((i & bit_test) == 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		j++;
+		bit_test = bit_test >> 1;
+		sleep(0.1);
+	}
+	return (0);
+}
---
+++ utils/write.c
@@ -0,0 +1,24 @@
+#include "../include/minitalk.h"
+
+// void	write_0(int *c)
+// {
+// 	*c = *c << 1;
+// }
+
+// void	write_1(char *c)
+// {
+// 	*c = *c | 1;
+// }
+
+void	write_0(int *c)
+{
+	*c = *c << 1;
+	printf("sigusr1 - %i", *c);
+}
+
+void	write_1(int *c)
+{
+	*c = *c << 1;
+	*c = *c | 1;
+	printf("sigusr2 - %i", *c);
+}
