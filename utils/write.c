d41e42761f724fc53c7f6835b8f4648cf7a3118f
Your Name <naldorsc2@gmail.com>
2024-07-16 05:00:45 +0200

    fuckud up, dont remember how this worked

---
+++ utils/write.c
@@ -0,0 +1,14 @@
+#include "../include/minitalk.h"
+
+void	write_0(char *c)
+{
+	*c = *c << 1;
+	printf("sigusr1 - %i", *c);
+}
+
+void	write_1(char *c)
+{
+	*c = *c << 1;
+	*c = *c | 1;
+	printf("sigusr2 - %i", *c);
+}
